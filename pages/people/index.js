23cb06e1fae963084aafc0d020525daaa14d3707
jdalzatec <jdalzatec@unal.edu.co>
2017-03-16 18:23:55 -0500

    Oscar is my friend

--- pages/people/index.js
+++ pages/people/index.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Helmet from 'react-helmet'
 import { config } from 'config'
+import { Grid, Row, Col , Image, Thumbnail, Button} from 'react-bootstrap'
+import '../../css/bootstrap.scss'
+
 
 export default class Index extends React.Component {
   render () {
@@ -22,7 +25,42 @@ export default class Index extends React.Component {
         
         <center><h2>The Bosses</h2></center>
         
-      
+        <Grid>
+    <Row>
+    <Col xs={6} md={4}>
+      <Thumbnail width={10} src="../images/fig_5.jpg" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Button</Button>&nbsp;
+          <Button bsStyle="default">Button</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail width={10} src="../images/fig_5.jpg" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Button</Button>&nbsp;
+          <Button bsStyle="default">Button</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail width={10} src="../images/fig_5.jpg" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Button</Button>&nbsp;
+          <Button bsStyle="default">Button</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    </Row>
+  </Grid>
+
+
         </div>
     )
   }
