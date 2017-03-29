26c76c5834ca1c544d56a23ecffa75db8b92832d
jdalzatec <jdalzatec@unal.edu.co>
2017-03-28 22:21:15 -0500

    Building vegas web page

--- pages/vegas/index.js
+++ pages/vegas/index.js
@@ -13,6 +13,28 @@ export default class Index extends React.Component {
 
         <center><img width={400} src="./images/vegas.png" alt=""/></center>
         
+        <p>
+          <b>𝕍egas</b> is a software package for simulation, graphics and analysis tools
+          for atomistic simulations of magnetic materials. This is hosted in
+          github <a className="link" href="https://github.com/jdalzatec/vegas" target="_blank">here</a>.
+        </p>
+
+        <p>
+          A simulation is composed by three parts essentially: sample building, computations and averages and plotting, where each part
+          is as important as the others. The philosophy of <b>𝕍egas</b> is to separate these parts in order to the simulation
+          is controlated by the user and don't by the software. Moreover, as it was said before, the part of averages and plotting
+          doesn't correspond to the computation part. For this reason, <b>𝕍egas</b> saves all simulation history (magnetization and
+          energy for each Monte Carlo Step for each step of field and temperature). This is usefull to compute critical exponents and
+          correlation functions, because for those things it is necessary to have the simulation history.
+        </p>
+
+        <p>
+          Hands on and let me to teach you with examples !.
+        </p>
+
+        <ul>
+          <li><Link className="link-bold" to={prefixLink('/vegas/installation/')}>Installation</Link></li>
+        </ul>
 
       </div>
     )
---
+++ pages/vegas/installation/index.md
@@ -0,0 +1,5 @@
+# Installation
+
+Until date, it is so easy to install **𝕍egas** in Ubuntu 14.* and 16.*. In the future. the installation will be so easy. We are working in this.
+
+In order to install **𝕍egas** you need to have installed in your computer the libraries for [HDF5](https://support.hdfgroup.org/HDF5/) and [JSON](https://en.wikibooks.org/wiki/JsonCpp). Let's talk about HDF5 and JSON. HDF5 is a data model, library, and file format for storing and managing data. Data is saved in binary such that the writing and reading is so fast. We use HDF5 because the simulation history usually is huge. If we would save the data in text, a simulation history could have a size until 30 Gb ... that is a bad idea. For this reason we decided to use HDF5. On the other hand, we used JSON to read the input for the simulation. It is a pretty format for inputs.
